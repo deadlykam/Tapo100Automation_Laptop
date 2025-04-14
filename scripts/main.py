264549d8cbefc1a263df02e19582ad8f6eb0be75
Kamran Wali <kamran369mars@gmail.com>
2025-04-14 19:44:15 +0100

    Documentation (WIP)

--- scripts/main.py
+++ scripts/main.py
@@ -1,8 +1,24 @@
+"""
+main.py
+
+This script starts the application.
+
+Dependencies:
+- PyQt5
+- main_ui (custom module)
+
+Author: Kamran Wali
+Date: 23-03-2025
+"""
+
 import sys
 from PyQt5.QtWidgets import QApplication
 from scripts.ui.main_ui import MainWindow
 
 def main():
+    """
+    Starts the application.
+    """
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
